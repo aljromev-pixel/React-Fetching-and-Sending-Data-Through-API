ca41fa0d0ff89e4dad9d26e2227edd21614cadfb
Jys_vGH <aljromev@gmail.com>
2026-08-23 15:29:15 +0800

    update the aboutpage

--- src/AboutPage.jsx
+++ src/AboutPage.jsx
@@ -3,12 +3,14 @@ import React from "react";
 function AboutPage() {
   return (
     <main>
-      <section className="page">
+       <section className="about-hero">
+        <p className="small-title">ABOUT THE PROJECT</p>
+
         <h1>About Us</h1>
 
         <p>
-          This project demonstrates the use of React components,
-          React Router, and React state.
+          Learning React by building a simple and functional
+          web application.
         </p>
       </section>
     </main>
